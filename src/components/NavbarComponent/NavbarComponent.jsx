1de5a7f003ce4d9ac34c8a905f3e0bae1dae6f28
Quoc Huy <nguyen7quoc4huy02@gmail.com>
2024-04-25 02:17:06 +0700

    Navbar

---
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { WrapperContent, WrapperLableText, WrapperTextPrice, WrapperTextValue } from './style'
+import { Checkbox, Rate } from 'antd';
+
+const NavbarComponent = () => {
+  const onChange = () => {}
+  const rederContent = (type, options) => {
+    switch (type) {
+      case 'text':
+        return options.map((option) => {
+            return (<WrapperTextValue>{option}</WrapperTextValue>) 
+        })
+      case 'checkbox':
+        return(
+          <Checkbox.Group style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '12px' }}onChange={onChange} >
+                {options.map((option) => {
+                  return(
+                    <Checkbox value={option.value}>{option.lable}</Checkbox>
+                  )
+                })}
+          </Checkbox.Group>
+        )
+      case 'star':
+        return options.map((option) => {
+                return(
+                  <div style={{ display: 'flex' }}>
+                    <Rate style={{ fontSize: '12px' }} disabled defaultValue={option} />
+                    <span style={{ marginLeft: '4px' }}>{`tu ${option} sao`}</span>
+                  </div>
+                )
+              })
+      case 'price':
+        return options.map((option) => {
+                return(
+                  <WrapperTextPrice>
+                    {option}
+                  </WrapperTextPrice>
+                )
+              })
+      default:
+        return {}
+    }
+  }
+  return (
+    <div>
+      <WrapperLableText>Lable</WrapperLableText>
+      <WrapperContent>
+        {rederContent('text', ['Tu lanh', 'TV', 'MAYGIAT'])}
+      </WrapperContent>
+      <WrapperContent>
+        {rederContent('checkbox', [{value: 'a', lable: 'A'},
+       { value: 'b', lable: 'B' }
+        ])}
+      </WrapperContent>
+      <WrapperContent>
+        {rederContent('star', [3, 4, 5])}
+      </WrapperContent>
+      <WrapperContent>
+        {rederContent('price', ['duoi 40', 'tren 50.000'])}
+      </WrapperContent>
+    </div>
+  )
+}
+
+export default NavbarComponent
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import slider1 from '../../assets/images/slider1.jpg'
 import slider2 from '../../assets/images/slider2.png'
 import slider3 from '../../assets/images/slider3.png'
 import CardComponent from '../../components/CardComponent/CardComponent'
+import NavbarComponent from '../../components/NavbarComponent/NavbarComponent'
 const HomePage = () => {
   const arr = ['TV', 'Tu lanh', 'Laptop',]
   return (
@@ -25,6 +26,7 @@ const HomePage = () => {
         <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center', gap: '20px' }}>
           <CardComponent/>
         </div>
+        <NavbarComponent/>
       </div>
     </>
   )
